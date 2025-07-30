3ca48bbc75cbfa57ee7c0ab42c7269e9022ba46b
Nontakorn CK <Nontakorn2424.chatk@gmail.com>
2025-07-30 16:07:56 +0700

    feat: add "Hello World" message to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
             />
             Deploy now
           </a>
+          <p>Hello World</p>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
